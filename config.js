16f3139445943d3263a06d320f11132e4a771604
btcyberpubli <btcyberpubli@gmail.com>
2026-01-06 14:42:28 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 // Global configuration for Prime Cleaning Services Ltd
 // Edit the phone number below to update it across the entire website
-const ukPhoneNumber = "+44 20 7946 0832";
+const ukPhoneNumber = "+44 7347940079";
+
